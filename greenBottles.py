2b24ec7a828e01d0cfb53a75335e20e9d067e180
Matt <mheavens7028@sohamcollege.org.uk>
2014-05-14 17:23:28 +0100

    Update greenBottles.py
    Here, I added timings, formats and a loop to make the code better.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,19 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+##10 GREEN BOTTLES - MATTHEW HEAVENS##
+
+import time
+bottles = 10
+sNeeded = "s"
+for x in range(10):
+    print("{0} green bottle{1}, hanging on the wall".format(bottles,sNeeded))
+    time.sleep(1)
+    print("{0} green bottle{1}, hanging on the wall".format(bottles,sNeeded))
+    time.sleep(1)
+    print("And if 1 green bottle should acidentally fall,")
+    time.sleep(1)
+    bottles = bottles-1
+    if bottles == 1:
+        sNeeded = ""
+    else:
+        sNeeded = "s"
+    print("They'll be {0} green bottle{1} hanging on the wall.".format(bottles,sNeeded))
+    time.sleep(1)
