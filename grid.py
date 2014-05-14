3368b75b30e9b6a490f76975f9ccf3ca41a37a54
Matt <mheavens7028@sohamcollege.org.uk>
2014-05-14 21:29:19 +0100

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,4 +1,8 @@
+##GRID PRINT - MATTHEW HEAVENS##
+
+question = int(input("How many rows do you want the grid to have? >>> "))
+for x in range(question):
+    print("-------")
+    print("|     |")
+
 print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
